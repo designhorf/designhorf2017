5c399f4107f8b8f90bb6e2517df9419da3df8308
designhorf <designhorf@gmail.com>
2017-06-04 17:25:27 +0200

    Skeleton: created basic React app

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Content from '../Content';  
+import './App.scss';
+
+export default function App() {  
+  return (
+    <div className="site-wrapper">
+      <Content />
+    </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {  
+  entry: [
+    path.join(__dirname, 'src/index.jsx'),
+  ],
+  output: {
+    path: path.join(__dirname, 'src/build'),
+    filename: 'build.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: path.join(__dirname, 'src/index.html')
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        use: [ 'babel-loader' ]
+      },
+      {
+        test: /\.(css|scss|sass)$/,
+        loader: 'style-loader!css-loader!sass-loader',
+      },
+    ]
+  },
+};
