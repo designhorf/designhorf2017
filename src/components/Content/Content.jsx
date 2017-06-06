627701f0853a19f648f1aef8eeaf80176f36f018
designhorf <designhorf@gmail.com>
2017-06-06 22:49:05 +0200

    Content: created simply Header and Body components

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
-// import './Content.scss';
+import './Content.scss';
 
-export default function Content() {  
-  return (
-    <div className="content">
-    </div>
-  );
+const CommitHeader = (props) => {
+	return <p className="italic">Commits on { props.date }</p>
+}
+
+const CommitBody = (props) => {
+	const { text } = props;
+
+	return (
+		<div className="commit-body">
+			<div className="img">
+				<img src="" alt=""/>
+			</div>
+			
+			<div className="text">
+				<h2>{ text }</h2>
+			</div>
+		</div>
+	)
+}
+
+export default function Content({ data }) {  
+	return (
+		<div className="content">
+			{
+				data.map(commit => 
+					<section key={ commit.id } className="commit-wrap">
+						<CommitHeader date={ commit.date } />
+						<CommitBody text={ commit.text } /> 
+						<p>designhorf</p>
+						<p>committed on Github XXX days ago</p>
+					</section>
+				)
+			}
+		</div>
+	);
 }
