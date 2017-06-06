8578e5eddba78a7eeecd703b5b5531a65f1eae71
designhorf <designhorf@gmail.com>
2017-06-06 22:46:22 +0200

    Data: passed test data to Content component in App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,23 @@ import React from 'react';
 import Content from '../Content';  
 import './App.scss';
 
+const commits = [
+		{
+			"id": 1,
+			"date": "30-04-2016",
+			"text": "commit text",
+			"isProject": true
+		},
+		{
+			"id": 2,
+			"date": "30-05-2016",
+			"text": "2. commit text",
+			"isProject": false
+		}
+];
+
 export default function App() {  
   return (
-    <div className="site-wrapper">
-      <Content />
-    </div>
+		<Content data={ commits }/>
   );
 }
