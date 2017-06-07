cd1bb74bf5adc8ae1a960f4efb901b3ec13494b9
designhorf <designhorf@gmail.com>
2017-06-07 19:12:10 +0200

    Project: added style to Projects

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import './Content.scss';
 
 const CommitHeader = (props) => {
-	return <p className="commit-date italic bold">Commits on { props.date }</p>
+	return <p className="commit-date italic bold commit-header">Commits on { props.date }</p>
 }
 
 const CommitBody = (props) => {
-	const { text } = props;
+	const { text, isProject } = props;
+	let classnames;
+
+	if (isProject) {
+		classnames = 'commit-body project';
+	} else {
+		classnames = 'commit-body';
+	}
 
 	return (
-		<div className="commit-body">
+		<div className={ classnames }>
 			<img className="profile-pic" src="./src/images/profile_pic.jpg" alt="profile pic"/>
 			
 			<div className="commit-text-wrap">
@@ -28,9 +35,9 @@ export default function Content({ data }) {
 		<div className="content">
 			{
 				data.map(commit => 
-					<section key={ commit.id } className="commit-wrap">
+					<section key={ commit.id } className="commit-wrap bottom-space">
 						<CommitHeader date={ commit.date } />
-						<CommitBody text={ commit.text } /> 
+						<CommitBody text={ commit.text } isProject={ commit.isProject } /> 
 					</section>
 				)
 			}
