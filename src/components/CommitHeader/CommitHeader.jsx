fff98bff4fd481f9116743c69d482d07090ed16a
designhorf <designhorf@gmail.com>
2017-06-10 15:15:17 +0200

    Refactor CommitHeader: separated CommitHeader as a component

---
+++ src/components/CommitHeader/CommitHeader.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './CommitHeader.scss';
+
+// const akarmi = ({ data }) => {
+// 	let commitsByMonth = {};
+	
+// 	data.forEach(commit => {
+// 		const date = commit.date.slice(0, 7);
+		
+// 		if (!commitsByMonth[date]) {
+// 			commitsByMonth[date] = [];
+// 		}
+
+// 		commitsByMonth[date].push(commit);
+// 	})
+// }
+
+export default function CommitHeader ({ index, data, month, year }) {
+	const existsHeader = (
+		index > 0 &&
+		data[index - 1].month === month &&
+		data[index - 1].year === year
+	);
+
+	if (existsHeader) {
+		return null;
+	}
+
+	return (
+		<div className="title top-space">
+			<div className="square"></div>
+			<p className="commit-date italic bold commit-header">Commits on { month }, { year }</p>
+		</div>
+	)
+}
