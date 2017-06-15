74b2030c513b64dd8a96861c6e0a1c6f35143753
designhorf <designhorf@gmail.com>
2017-06-15 14:08:21 +0200

    Design: created Design component

---
+++ src/pages/Design/Design.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PageTitle from './../../components/PageTitle';
+// import './Design.scss';
+
+export default function Design() {  
+	return (
+		<div className="content">
+			<PageTitle title = 'Design' />
+		</div>
+	);
+}
