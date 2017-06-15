8ab5a34e4c8b29c380b7ea9362847b7ea6e39087
designhorf <designhorf@gmail.com>
2017-06-15 14:08:49 +0200

    Home: created Home component

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PageTitle from './../../components/PageTitle';
+
+export default function Home() {  
+	return (
+		<div className="content">
+			<PageTitle title = 'Home' />
+		</div>
+	);
+}
