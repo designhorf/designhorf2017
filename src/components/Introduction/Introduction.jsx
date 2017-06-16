aca3d6e7365abb82ba0771bdd4bf88dce76cce94
designhorf <designhorf@gmail.com>
2017-06-16 08:15:56 +0200

    Introduction: created Introduction component

---
+++ src/components/Introduction/Introduction.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Introduction.scss';
+
+export default function Introduction () {
+    return (
+        <div className="home-header">
+            <h1>Hi, I'm</h1>
+            <h1>Ferenc Horvath</h1>
+            <h3>Front-end developer & UI desegner</h3>
+        </div>
+    )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import PageTitle from './../../components/PageTitle';
+import { Link } from 'react-router-dom';
+import Introduction from './../../components/Introduction';
+
+const Button = ({ buttonText }) => {
+    return (
+        <button className="btn">
+            { buttonText }
+        </button>
+    )
+}
 
 export default function Home() {  
 	return (
 		<div className="content">
-			<PageTitle title = 'Home' />
+			<Introduction />
+			<div>
+				<Link to='/design'>
+                	<Button buttonText="Design" />
+				</Link>
+                <img src="" alt=""/>
+				<Link to='/code'>
+                	<Button buttonText="Code" />
+				</Link>
+            </div>
 		</div>
 	);
 }
