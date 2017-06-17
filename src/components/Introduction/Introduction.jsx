a45fb38dcac6a9cf00508e17441f468e75cc042d
designhorf <designhorf@gmail.com>
2017-06-17 20:09:06 +0200

    fixed some typo mistakes

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -4,9 +4,9 @@ import './Introduction.scss';
 export default function Introduction () {
     return (
         <div className="home-header">
-            <h1>Hi, I'm</h1>
+            <h1>Hi, I am</h1>
             <h1>Ferenc Horvath</h1>
-            <h3>Front-end developer & UI desegner</h3>
+            <h3>Front-end developer & UI designer</h3>
         </div>
     )
 }
