04932daa43eac7976020375f7b7490335de02a78
designhorf <designhorf@gmail.com>
2017-06-21 11:30:44 +0200

    MenuElement CSS: arranged css properties in Navigation and created scss file for MenuElement as well

--- src/components/MenuElement/MenuElement.jsx
+++ src/components/MenuElement/MenuElement.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import './MenuElement.scss';
 
 export default function MenuElement ({linkTarget, menuText})  {
     return (
