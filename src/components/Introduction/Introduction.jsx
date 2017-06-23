47c0b9174bc1c38862177adface2ccb562e55219
designhorf <designhorf@gmail.com>
2017-06-23 18:54:57 +0200

    Introduction: Fixed padding issue

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -4,11 +4,15 @@ import './Introduction.scss';
 export default function Introduction () {
     return (
         <div className="home-header">
-            <h1 className="introduction-1 bg-green">Hi, I am</h1>
-            <br/>
-            <h1 className="introduction-1 bg-green">Ferenc Horvath</h1>
-            <br/>
-            <h3 className="introduction-3 bg-green">Front-end developer & UI designer</h3>
+            <div className="introduction-box">
+                <span className="introduction-1 bg-green">Hi, I am</span>
+            </div>
+            <div className="introduction-box">
+                <span className="introduction-1 bg-green">Ferenc Horvath</span>
+            </div>
+            <div className="introduction-box">
+                <span className="introduction-3 bg-green">Front-end developer & UI designer</span>
+            </div>
         </div>
     )
 }
