9072f78678962b8da36cc00f9d0cf9a6e38dac56
designhorf <designhorf@gmail.com>
2017-06-26 06:12:42 +0200

    References: created new component for design references

---
+++ src/components/References/References.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function References ({ data }) {
+    return (
+        <div>
+            { console.log(data) }
+        </div>
+    )
+}
--- src/pages/Design/Design.jsx
+++ src/pages/Design/Design.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import PageTitle from './../../components/PageTitle';
 import Tools from './../../components/Tools';
+import References from './../../components/References';
 // import './Design.scss';
 
 export default function Design() {  
+	const designs = [
+		{
+			"text": 'Logo design for Varda-Véd Ltd.',
+			"year": '2014',
+			"month": 'July',
+			"tools": ['illustrator'],
+			"images": ['vardaved.jpg']
+		}
+	];
+	
 	return (
 		<div className="content">
 			<PageTitle title = 'Design' />
 			<Tools />
+			<References data={ designs }/>
 		</div>
 	);
 }
