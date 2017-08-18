a067457e9d6772f2cfdebc0e86927aa6d916b735
Ferenc Horvath <ferhor@MT-STUDIO-0001.local>
2017-08-18 11:01:49 +0200

    change moment.js to vanilla solution

--- src/components/CommitBody/CommitBody.jsx
+++ src/components/CommitBody/CommitBody.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import moment from 'moment';
 import ProfilePic from './profile_pic.jpg';
 import './CommitBody.scss';
 
 export default function CommitBody ({ index, isProject, text, date }) {
-	const m = moment({date}.date),
-		today = moment().startOf('day'),
-		days = Math.round(moment.duration(today - m).asDays()),
-		months = Math.round(moment.duration(today - m).asMonths()),
-		years = Math.round(moment.duration(today - m).asYears());
+	const year = {date}.date.slice(0,4),
+		month = {date}.date.slice(5),
+		startDate = new Date(year, month - 1),
+		today = new Date(),
+		_MS_PER_DAY = 1000 * 60 * 60 * 24;
+	
+	function dateDiffInDays(a, b) {
+	  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate()),
+	  		utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+	
+	  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+	}
 
 	return (
 		<div key={ index } className={ getClassName(isProject) }>
@@ -17,7 +23,11 @@ export default function CommitBody ({ index, isProject, text, date }) {
 				<p className="commit-title">{ text }</p>
 				<div className="commit-small-texts">
 					<b className="username">designhorf</b>
-					<p className="committed-ago">committed on Github { days < 365 ? days + ' days' : years + ' years' } ago</p>
+					<p className="committed-ago">committed on Github { 
+						dateDiffInDays(startDate, today) < 365 
+							? dateDiffInDays(startDate, today) + ' days' 
+							: Math.round(dateDiffInDays(startDate, today)/365) + ' years' 
+						} ago</p>
 				</div>
 			</div>
 		</div>
