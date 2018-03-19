d5d31044a1249264512962606f0eace60ac4fa03
designhorf <designhorf@gmail.com>
2018-03-19 06:32:00 +0100

    Refactored Button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Button.scss';
 
-export default function Button ({ buttonText, buttonClass }) {
+export default function Button ({ text, btnClass, link }) {
     return (
-        <button className={ ( buttonClass ? 'btn ' + buttonClass : 'btn' ) }>
-            { buttonText }
-        </button>
+        <Link className={( 'link ' + 'link-' + text.toLowerCase() )} to={ link }>
+            <button className={ ( btnClass ? 'btn ' + btnClass : 'btn' ) }>
+                { text }
+            </button>
+        </Link>
     )
 }
