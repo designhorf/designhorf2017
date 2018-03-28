b828d31fa9141dd9dd502ed20c4805658f3250c7
designhorf <designhorf@gmail.com>
2018-03-28 06:36:32 +0200

    production build

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,9 @@ module.exports = {
             removeRedundantAttributes: true
         }
     }),
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+    }),
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.AggressiveMergingPlugin()
   ],
